770d9c4f4eb4b6d8988e9e315524e90d8e333dd5
toto <2345672@cegepmontpetit.ca>
2024-12-12 21:42:23 -0500

    FCT_Migration

--- CEGES_TP1/CEGES_DataAccess/CEGESDbContext.cs
+++ CEGES_TP1/CEGES_DataAccess/CEGESDbContext.cs
@@ -22,47 +22,47 @@ namespace CEGES_DataAccess.data
         public DbSet<Lineaire> Lineaires { get; set; }
         public DbSet<EmissionMensuelle> EmissionMensuelles { get; set; }
 
-        // Cette méthode permet d'ajouter des données par défaut dans la base de données
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder); // Appeler la méthode de base
+        //Cette méthode permet d'ajouter des données par défaut dans la base de données
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder); // Appeler la méthode de base
 
-            // Ajouter des données de seed pour l'entité 'Entreprise'
-            modelBuilder.Entity<Entreprise>().HasData(
-                new Entreprise { id =1, Nom = "Entreprise A" },
-                new Entreprise { id = 2, Nom = "Entreprise B" }
-            );
+        //    Ajouter des données de seed pour l'entité 'Entreprise'
+        //    modelBuilder.Entity<Entreprise>().HasData(
+        //        new Entreprise { id = 1, Nom = "Entreprise A" },
+        //        new Entreprise { id = 2, Nom = "Entreprise B" }
+        //    );
 
-            // Ajouter des données de seed pour l'entité 'Groupe'
-            modelBuilder.Entity<Groupe>().HasData(
-                new Groupe { id = 1, Nom = "Groupe A", NombreGroupe = 1 },
-                new Groupe { id = 2, Nom = "Groupe B", NombreGroupe = 2 }
-            );
+        //    Ajouter des données de seed pour l'entité 'Groupe'
+        //    modelBuilder.Entity<Groupe>().HasData(
+        //        new Groupe { id = 1, Nom = "Groupe A", NombreGroupe = 1 },
+        //        new Groupe { id = 2, Nom = "Groupe B", NombreGroupe = 2 }
+        //    );
 
-            // Ajouter des données de seed pour l'entité 'Equipement'
-            modelBuilder.Entity<Equipement>().HasData(
-                new Equipement { id = 1, TypeEquipement = "Equipement 1" },
-                new Equipement { id = 2, TypeEquipement= "Equipement 2" }
-            );
+        //    Ajouter des données de seed pour l'entité 'Equipement'
+        //    modelBuilder.Entity<Equipement>().HasData(
+        //        new Equipement { id = 1, TypeEquipement = "Equipement 1" },
+        //        new Equipement { id = 2, TypeEquipement = "Equipement 2" }
+        //    );
 
-            // Ajouter des données de seed pour l'entité 'Constante'
-            modelBuilder.Entity<Constante>().HasData(
-                new Constante { id= 1, TypeEquipement= "Constante A", Quantite = 100 },
-                new Constante { id = 2, TypeEquipement = "Constante B", Quantite = 200 }
-            );
+        //    Ajouter des données de seed pour l'entité 'Constante'
+        //    modelBuilder.Entity<Constante>().HasData(
+        //        new Constante { id = 1, TypeEquipement = "Constante A", Quantite = 100 },
+        //        new Constante { id = 2, TypeEquipement = "Constante B", Quantite = 200 }
+        //    );
 
-            // Ajouter des données de seed pour l'entité 'Lineaire'
-            modelBuilder.Entity<Lineaire>().HasData(
-                new Lineaire { id = 1, UniteMesure = "m", FacteurConversion = 1.5 },
-                new Lineaire { id = 2, UniteMesure = "cm", FacteurConversion = 0.01 }
-            );
+        //    Ajouter des données de seed pour l'entité 'Lineaire'
+        //    modelBuilder.Entity<Lineaire>().HasData(
+        //        new Lineaire { id = 1, UniteMesure = "m", FacteurConversion = 1.5 },
+        //        new Lineaire { id = 2, UniteMesure = "cm", FacteurConversion = 0.01 }
+        //    );
 
-            // Configuration pour ajouter des données de seed
-            modelBuilder.Entity<EmissionMensuelle>().HasData(
-                new EmissionMensuelle { id = 1, Mois = "Janvier", Annee = 2023, TotalEntreprise = 1000 },
-                new EmissionMensuelle { id = 2, Mois = "Février", Annee = 2023, TotalEntreprise = 1200 })
-          ;
-        }
+        //    Configuration pour ajouter des données de seed
+        //    modelBuilder.Entity<EmissionMensuelle>().HasData(
+        //        new EmissionMensuelle { id = 1, Mois = "Janvier", Annee = 2023, TotalEntreprise = 1000 },
+        //        new EmissionMensuelle { id = 2, Mois = "Février", Annee = 2023, TotalEntreprise = 1200 })
+        //  ;
+        //}
     }
 }
     
