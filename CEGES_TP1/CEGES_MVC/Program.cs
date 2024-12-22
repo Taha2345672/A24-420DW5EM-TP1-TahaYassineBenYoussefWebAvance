f607669a3e45ee7d579c7319e01e48942f15197a
Taha234672 <2345672@cegepmontpetit.ca>
2024-12-21 19:48:07 -0500

    bugfix

--- CEGES_TP1/CEGES_MVC/Program.cs
+++ CEGES_TP1/CEGES_MVC/Program.cs
@@ -1,42 +1,35 @@
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using CEGES_DataAccess;
 using CEGES_Services;
 using CEGES_Service;
 
-
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<CEGESDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Configuration de la base de données
+builder.Services.AddDbContext<CEGESDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-
-builder.Services.AddControllersWithViews()
-.Services.AddDbContext<CEGESDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+// Ajout des services pour DI
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped(typeof(IServiceBaseAsync<>), typeof(ServiceBase<>));
-builder.Services.AddScoped<IEntrepriseService, IEntrepriseService>();
+builder.Services.AddScoped<IEntrepriseService, EntrepriseService>(); // Utilisation de l'implémentation correcte
 builder.Services.AddScoped<IGroupeService, GroupeService>();
 builder.Services.AddScoped<IEquipementConstantesService, EquipementConstantesService>();
 builder.Services.AddScoped<IEquipementLineairesService, EquipementLineairesService>();
 builder.Services.AddScoped<IEquipementRelativesService, EquipementRelativesService>();
 
-
-
+// Configuration des caches distribués (si nécessaire)
 builder.Services.AddDistributedMemoryCache();
 
 var app = builder.Build();
 
-
+// Gestion des erreurs
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -44,20 +37,22 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); // HSTS pour les environnements de production
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+// Configuration des routes
 app.MapControllerRoute(
     name: "Configuration",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-app.MapAreaControllerRoute(
+
+app.MapControllerRoute(
     name: "default",
-    areaName: "Configuration",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Démarrage de l'application
 app.Run();
